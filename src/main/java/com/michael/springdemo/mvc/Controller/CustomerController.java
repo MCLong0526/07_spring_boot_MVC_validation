781eae3441c8460c361895185259b5e62c0aae03
Michael526-dev <michael.chia@bunkerchain.io>
2025-09-29 08:13:16 +0800

    add some fields for validation learn

--- src/main/java/com/michael/springdemo/mvc/Controller/CustomerController.java
+++ src/main/java/com/michael/springdemo/mvc/Controller/CustomerController.java
@@ -3,10 +3,13 @@ package com.michael.springdemo.mvc.Controller;
 import com.michael.springdemo.mvc.Domain.Customer;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,6 +20,17 @@ public class CustomerController {
 
     @Value("${places}")
     private List<String> places;
+
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+
+        // this line is removes the leading and trailing spaces from the form input
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        // this line is to let spring know that as long as is string, clean up first before put it in object.
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
--- src/main/java/com/michael/springdemo/mvc/Domain/Customer.java
+++ src/main/java/com/michael/springdemo/mvc/Domain/Customer.java
@@ -1,7 +1,6 @@
 package com.michael.springdemo.mvc.Domain;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
@@ -12,11 +11,33 @@ public class Customer {
 
     private String place;
 
+    @Min(value=0, message="must be greater than zero")
+    @Max(value=10, message="must be smaller than 10")
+    private int freePasses;
+
+    @NotNull(message="is required")
+    @Pattern(regexp = "^[0-9]+$", message = "Only digits allowed")
+    @Size(min = 10, max = 15, message = "Phone number must be 10-15 digits")
+    private String phoneNumber;
+
+    @NotNull(message="is required")
+    @Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digit")
+    private String postalCode;
+
+    @NotNull(message="is required")
+    @Pattern(regexp="^[0-9]+$", message="only digits is allowed")
+    @Size(max=3, message="age should not more than 3 digits")
+    private String age;
+
     public Customer(){}
-    public Customer(String firstName, String lastName, String place) {
+    public Customer(String firstName, String lastName, String place, int freePasses, String phoneNumber, String postalCode, String age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.place=place;
+        this.freePasses = freePasses;
+        this.phoneNumber = phoneNumber;
+        this.postalCode = postalCode;
+        this.age=age;
     }
 
     public String getFirstName() {
@@ -41,5 +62,37 @@ public class Customer {
 
     public void setPlace(String place) {
         this.place = place;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
 }
